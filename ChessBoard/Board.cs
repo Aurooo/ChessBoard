d69a40e65518647b595250082fb1dd68a2797248
Aurooo <aurom140@gmail.com>
2021-08-09 23:07:40 +0200

    allowed moves for each piece on the board - complete

---
+++ ChessBoard/Board.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoard
+{
+    public class Board
+    {
+        public int Size { get; set; }
+        public Cell[,] Grid { get; set; }
+
+        public Board (int s)
+        {
+            Size = s;
+
+            Grid = new Cell[Size, Size];
+
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    Grid[i, j] = new Cell(i, j);
+                }
+            }
+        }
+
+        public void MarkNextLegalMove(Cell current, string chessPiece)
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    Grid[i, j].LegalNextMove = false;
+                    Grid[i, j].CurrentlyOccupied = false;
+                }
+            }
+
+            switch (chessPiece.ToLower())
+            {
+                case "knight":
+                    if (current.RowNumber + 2 < Size && current.ColumnNumber + 1 < Size)
+                        Grid[current.RowNumber + 2, current.ColumnNumber + 1].LegalNextMove = true;
+                    if (current.RowNumber + 2 < Size && current.ColumnNumber - 1 >= 0)
+                        Grid[current.RowNumber + 2, current.ColumnNumber - 1].LegalNextMove = true;
+                    if (current.RowNumber - 2 >= 0 && current.ColumnNumber + 1 < Size)
+                        Grid[current.RowNumber - 2, current.ColumnNumber + 1].LegalNextMove = true;
+                    if (current.RowNumber - 2 >= 0 && current.ColumnNumber - 1 >= 0)
+                        Grid[current.RowNumber - 2, current.ColumnNumber - 1].LegalNextMove = true;
+                    if (current.RowNumber + 1 < Size && current.ColumnNumber + 2 < Size)
+                        Grid[current.RowNumber + 1, current.ColumnNumber + 2].LegalNextMove = true;
+                    if (current.RowNumber + 1 < Size && current.ColumnNumber - 2 >= 0)
+                        Grid[current.RowNumber + 1, current.ColumnNumber - 2].LegalNextMove = true;
+                    if (current.RowNumber - 1 >= 0 && current.ColumnNumber + 2 < Size)
+                        Grid[current.RowNumber - 1, current.ColumnNumber + 2].LegalNextMove = true;
+                    if (current.RowNumber - 1 >= 0 && current.ColumnNumber - 2 >= 0)
+                        Grid[current.RowNumber - 1, current.ColumnNumber - 2].LegalNextMove = true;
+                    break;
+
+                case "king":
+                    if (current.ColumnNumber + 1 < Size)
+                        Grid[current.RowNumber, current.ColumnNumber + 1].LegalNextMove = true;
+                    if (current.ColumnNumber - 1 >= 0)
+                        Grid[current.RowNumber, current.ColumnNumber - 1].LegalNextMove = true;
+                    if (current.RowNumber + 1 < Size)
+                        Grid[current.RowNumber + 1, current.ColumnNumber].LegalNextMove = true;
+                    if (current.RowNumber + 1 < Size && current.ColumnNumber + 1 < Size)
+                        Grid[current.RowNumber + 1, current.ColumnNumber + 1].LegalNextMove = true;
+                    if (current.RowNumber + 1 < Size && current.ColumnNumber - 1 >= 0)
+                        Grid[current.RowNumber + 1, current.ColumnNumber - 1].LegalNextMove = true;
+                    if (current.RowNumber - 1 >= 0)
+                        Grid[current.RowNumber - 1, current.ColumnNumber].LegalNextMove = true;
+                    if (current.RowNumber - 1 >= 0 && current.ColumnNumber + 1 < Size)
+                        Grid[current.RowNumber - 1, current.ColumnNumber + 1].LegalNextMove = true;
+                    if (current.RowNumber - 1 >= 0 && current.ColumnNumber - 1 >= 0)
+                    Grid[current.RowNumber - 1, current.ColumnNumber - 1].LegalNextMove = true;
+                    break;
+
+                case "rook":
+                    for (int i = 0; i < Size; i++)
+                        Grid[i, current.ColumnNumber].LegalNextMove = true;
+                    for (int i = 0; i < Size; i++)
+                        Grid[current.RowNumber, i].LegalNextMove = true;
+                    break;
+
+                case "bishop":
+                    for (int i = current.RowNumber, j = current.ColumnNumber; i >= 0 && j >= 0; i--, j--)
+                        Grid[i, j].LegalNextMove = true;
+                    for (int i = current.RowNumber, j = current.ColumnNumber; i < Size && j < Size; i++, j++)
+                        Grid[i, j].LegalNextMove = true;
+                    for (int i = current.RowNumber, j = current.ColumnNumber; i >= 0 && j < Size; i--, j++)
+                        Grid[i, j].LegalNextMove = true;
+                    for (int i = current.RowNumber, j = current.ColumnNumber; i < Size && j >= 0; i++, j--)
+                        Grid[i, j].LegalNextMove = true;
+                    break;
+
+                case "queen":
+                    for (int i = 0; i < Size; i++)
+                        Grid[i, current.ColumnNumber].LegalNextMove = true;
+                    for (int i = 0; i < Size; i++)
+                        Grid[current.RowNumber, i].LegalNextMove = true;
+                    for (int i = current.RowNumber, j = current.ColumnNumber; i >= 0 && j >= 0; i--, j--)
+                        Grid[i, j].LegalNextMove = true;
+                    for (int i = current.RowNumber, j = current.ColumnNumber; i < Size && j < Size; i++, j++)
+                        Grid[i, j].LegalNextMove = true;
+                    for (int i = current.RowNumber, j = current.ColumnNumber; i >= 0 && j < Size; i--, j++)
+                        Grid[i, j].LegalNextMove = true;
+                    for (int i = current.RowNumber, j = current.ColumnNumber; i < Size && j >= 0; i++, j--)
+                        Grid[i, j].LegalNextMove = true;
+                    break;
+                default:
+                    break;
+            }
+
+            Grid[current.RowNumber, current.ColumnNumber].CurrentlyOccupied = true;
+        }
+    }
+}
---
+++ ChessBoard/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChessBoard
+{
+    class Program
+    {
+        static Board myBoard = new Board(8);
+        static void Main(string[] args)
+        {
+            
+            Cell currentCell = SetCurrentCell();
+            string piece = SetPiece();
+            
+
+            myBoard.MarkNextLegalMove(currentCell, piece);
+            printBoard(myBoard);
+        }
+
+        private static void printBoard(Board myBoard)
+        {
+            Console.WriteLine("(r)+---+---+---+---+---+---+---+---+");
+            for (int i = 0; i < myBoard.Size; i++)
+            {
+                Console.Write($"({i})");
+                for (int j = 0; j < myBoard.Size; j++)
+                {
+                    Cell c = myBoard.Grid[i, j];
+                    
+                    if (c.CurrentlyOccupied == true)
+                    {
+                        Console.Write("| X ");
+                    }
+                    else if (c.LegalNextMove == true)
+                    {
+                        Console.Write("| + ");
+                    }
+                    else
+                    {
+                        Console.Write("|   ");
+                    }
+                }
+                Console.Write("|\n");
+                Console.WriteLine("   +---+---+---+---+---+---+---+---+");
+            }
+            Console.WriteLine("(c)+(0)+(1)+(2)+(3)+(4)+(5)+(6)+(7)");
+            Console.WriteLine("==================");
+        }
+        private static Cell SetCurrentCell()
+        {
+            int row;
+            int column;
+            do
+            {
+                Console.WriteLine("Enter row number (0-7)");
+            } while (!(int.TryParse(Console.ReadLine(), out row) && row >= 0 && row < 7));
+
+            do
+            {
+                Console.WriteLine("Enter column number (0-7)");
+            } while (!(int.TryParse(Console.ReadLine(), out column) && column >= 0 && column < 7));
+
+            myBoard.Grid[row, column].CurrentlyOccupied = true;
+
+            return myBoard.Grid[row, column];
+        }
+        private static string SetPiece()
+        {
+            string piece;
+            List<string> pieces = new List<string>() { "rook", "king", "queen", "bishop", "knight" };
+            do
+            {
+                Console.WriteLine("Enter piece");
+                piece = Console.ReadLine();
+            } while (!pieces.Contains(piece));
+            return piece;
+        }
+    }
+}
